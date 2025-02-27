bac7f05d62acd46c7ff31944959e43d8f033e80d
arcenioCorteis <arcenioCorteis@refera.com.br>
2025-02-27 18:58:38 +0200

    feat: add sideBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Header } from "./components/Header";
 import { Post } from "./Posts";
 import "./global.css";
+import styles from "./App.module.css";
+import { SideBar } from "./components/SideBar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Corteis"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post
+            author="Corteis"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
         Consequuntur recusandae eveniet eius, temporibus, quas ex fugit nam ipsa voluptatibus quia sequi cumque odio aperiam, voluptates id quasi amet unde qui? "
-      />
-      <Post
-        author="Francisco"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          />
+          <Post
+            author="Francisco"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
         Consequuntur recusandae eveniet eius, temporibus, quas ex fugit nam ipsa voluptatibus quia sequi cumque odio aperiam, voluptates id quasi amet unde qui? "
-      />
+          />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,29 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from "./SideBar.module.css";
+
+export function SideBar() {
+  return (
+    <aside className={styles.sideBar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1566837945700-30057527ade0?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://media.licdn.com/dms/image/v2/D4E03AQGPCdkxcHNoYw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1672492101503?e=1746057600&v=beta&t=qHy6OQZB84APvrdh1w3qj_ke1VBUIozDgtUMn4ISW4M"
+          alt=""
+        />
+        <strong>Corteis Junior</strong>
+        <span>FullStack Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
